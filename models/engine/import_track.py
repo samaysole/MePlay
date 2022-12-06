6c6d8c517c90b0848b0764ba648a99093b1b4309
skahsay <skahsay@paga.com>
2022-12-06 23:09:15 +0300

    updated

--- models/engine/import_track.py
+++
@@ -1,7 +0,0 @@
-from spotify_client import *
-
-client_id = 'xxxxxxxx'
-client_secret = 'xxxxxxxx'
-
-spotify = SpotifyAPI(client_id, client_secret)
-spotify.search({"track": "Time"}, search_type="track")
